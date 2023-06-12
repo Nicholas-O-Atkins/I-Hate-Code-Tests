d4d5d7b44e388597aedf477aa32899a510320060
Nicholas Atkins <37705884+Nicholas-O-Atkins@users.noreply.github.com>
2023-06-12 01:42:10 -0230

    Moving up
    Continuing the tutorial video

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) =>{
-    res.send('Hello World')
+    const articles = [{
+        title: 'Test Title',
+        createdAt: Date.now(),
+        description: 'Test Descript'
+    }]
+    res.render('index', {articles: articles})
 })
 
 app.listen(5000)
