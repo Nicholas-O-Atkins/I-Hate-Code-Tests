8f920bf02a3f3d9974806dabb11bdb07f56bec6c
Nicholas Atkins <37705884+Nicholas-O-Atkins@users.noreply.github.com>
2023-07-09 17:08:42 -0230

    Got it working again
    40.16 in the video

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 
 const app = express()
@@ -10,16 +11,8 @@ app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false}))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: "Test",
-        createdAt: new Date(),
-        description: "Test desc"
-    },{
-        title: "Test2",
-        createdAt: new Date(),
-        description: "Test desc 2" 
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find()
     res.render('articles/index', { articles: articles })
 })
 
