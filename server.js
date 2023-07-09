54d0fb281551f6d79e88642b5aaae82f0d02275f
Nicholas Atkins <37705884+Nicholas-O-Atkins@users.noreply.github.com>
2023-07-09 16:37:01 -0230

    Still working so far

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 
 const app = express()
 
+mongoose.connect('mongodb://127.0.0.1/webserver1')
+
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) => {
     const articles = [{
