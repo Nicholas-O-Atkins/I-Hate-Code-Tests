b3fa3f1fcb3959c49ff5729e1aca27fe584bc3f5
Nicholas Atkins <37705884+Nicholas-O-Atkins@users.noreply.github.com>
2024-02-24 18:17:29 -0330

    Attempting to create DCS in ejs
    Going to be adding more files to mimic the file structure of the thing I am building at work, since I believe ASP.NET Framework is limiting options for improvements with my lack of ASP knowledge and this will allow for more freedom in creating the front end, if MongoDB doesn't work out I will be attempting to use Postgres

---
+++ routes/recipes.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const Article = require('./../models/article')
+const router = express.Router()
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
+
+// Declare the routers and models
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+
+const Recipe = require('./models/recipe')
+const recipeRouter = require('./routes/recipes')
+
+// Method overrides
 const methodOverride = require('method-override')
 
 const app = express()
-
+// Mongoose connection
 mongoose.connect('mongodb://127.0.0.1/webserver1')
-
+// view engine for rendering
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false}))
 app.use(methodOverride('_method'))
 
+// Setting the deafault to index of Article 
+// TODO change to index for DCS homepage
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({
         createdAt: 'desc'
@@ -21,5 +30,6 @@ app.get('/', async (req, res) => {
 })
 
 app.use('/articles', articleRouter)
+app.use('/recipes', recipeRouter)
 
 app.listen(5000)
